0679b429c8aa83ffcd66768a029214b8a9d7bf28
Durabun <eugenejon1@gmail.com>
2017-08-05 03:54:23 -0400

    Used a loop system to find the X values of the roots

--- rootFinder.py
+++ rootFinder.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from os import system
 from scipy.optimize import*
@@ -16,25 +15,6 @@ def Evenroot(x,r):
 def Oddroot(x,r):
 	return -x*((np.cos(x))/(np.sin(x)))-np.sqrt(r**2-x**2)
 
-#This fn is for determining the value of the even root curve. It is in fact non dimensionalized
-def ErootY(x):
-        return x*(np.sin(x))/(np.cos(x))
-
-#This is the fn for determining the value of the odd root curve.
-def OrootY(x):
-        return -x*(np.cos(x))/(np.sin(x))
-
-#This is to determine the propagation "K" vector. Units are in inverse Angstrom
-def PropVec(x,a):
-    return x/(a*10**(10))
-
-#This is to determine the tunneling "k" vector (yeah, notation sucks). Units are in inverse Angstrom
-def TunVec(y,a):
-    return y/(a*10**(10))
-
-#This function is for determining the energy of each state.
-def Energy(y,p,V):
-    return V*(y/p)**2
 
 #The function below determines the radius of possible roots based on the parameters of the particle and the potential well
 def radius(m,a,V):
@@ -45,61 +25,70 @@ R = radius((206.8*9.11*10**(-31)),(4.69*10**(-11)),(1.6022*10**(-19)))
 
 i = 1
 j = 0
-while R > i*np.pi:
+while 1.1*np.pi > i*np.pi:
 	i=i+1
-while R > (1+2*j)*np.pi/2:
+while 1.1*np.pi > (1+2*j)*np.pi/2:
 	j = j+1
 
-print(str(i+j)+" root(s) in total.")
+print str(i+j)+" root(s) in total."
 
 
-evenRootX = np.zeros(i)
-evenRootY = np.zeros(i)
-oddRootX = np.zeros(j)
-oddRootY = np.zeros(j)
-propagationVector = np.zeros(i+j)
-tunnelVector = np.zeros(i+j)
+evenRoot = np.zeros(i)
+oddRoot = np.zeros(j)
 
 ie = 0
 jo = 0
-ip = 0 #propagation vector index
-it = 0 #tunnel vector index
-dx = 0.1
-
+dx = 0.01
+x0 = 0.01
 #Test Cases for the initial case I worked on in my final
 # In this case, there are 2 even roots and 1 odd root
 
+#These are test root functions
+#When I figure out how to make a function dynamic i.e. changing "r"
+#Then I will go back to using the original root functions
 def rootT(x):
-	return x*((np.sin(x))/(np.cos(x)))-np.sqrt((4)**2-x**2)
+	return x*((np.sin(x))/(np.cos(x)))-np.sqrt((1.1*np.pi)**2-x**2)
 
 def OrootT(x):
-	return -x*((np.cos(x))/(np.sin(x)))-np.sqrt((4)**2-x**2)
-evenRootX[ie] = newton(rootT,1+dx)
-evenRootY[ie] = ErootY(evenRootX[ie])
-propagationVector[ip] = PropVec(evenRootX[ie],(4.69*10**(-11)))
-tunnelVector[it] = TunVec(evenRootY[ie],(4.69*10**(-11)))
+	return -x*((np.cos(x))/(np.sin(x)))-np.sqrt((1.1*np.pi)**2-x**2)
 
+while np.absolute(rootT(x0)) > 0.5:
+	x0 = x0+dx
+evenRoot[ie] = newton(rootT,x0)
 #print newton(rootT,np.pi)
 #print newton(OrootT,0.5*np.pi+3*dx)
 #print newton(OrootT,1.5*np.pi+dx)
 
 ie = 1
-while i>ie:
-	if R > ie*np.pi:
-            evenRootX[ie] = newton(rootT,ie*np.pi+dx)
-            evenRootY[ie] = ErootY(evenRootX[ie])
-            ie = ie+1
+x0 = (1+2*jo)*np.pi/2
+#while i>ie:
+#	if R > ie*np.pi:
+#		evenRoot[ie] = newton(rootT,ie*np.pi+dx)	
+#		ie = ie+1
+#while j>jo:
+#	if R > (1+2*jo)*np.pi/2:
+#		oddRoot[jo] = newton(OrootT,(1+2*jo)*np.pi/2+2*2*dx)
+#		jo=jo+1
 
-while j>jo:
-	if R > (1+2*jo)*np.pi/2:
-            oddRootX[jo] = newton(OrootT,(1+2*jo)*np.pi/2+2*2*dx)
-            oddRootY[jo] = OrootY(oddRootX[jo])
-            jo=jo+1
+#This loop is for determing the X coords of the even and odd roots
+#Which are then saved in appropriate arrays
+while x0<1.1*np.pi:
+	while np.absolute(OrootT(x0)) > 0.25:
+		x0=x0+dx
+	oddRoot[jo] = newton(OrootT,x0)
+	jo=jo+1
+	x0 = ie*np.pi
+	print x0
+	if x0<1.1*np.pi:
+		while np.absolute(rootT(x0)) > 0.25:
+			x0 = x0+dx
+		evenRoot[ie] = newton(rootT,x0)
+		ie = ie+1
+		x0 = (1+2*jo)*np.pi/2
+		print x0
+	
 
+print evenRoot
+print oddRoot
+print x0
 
-print (evenRootX)
-print (evenRootY)
-print (oddRootX)
-print (oddRootY)
-print (propagationVector)
-print(tunnelVector)
