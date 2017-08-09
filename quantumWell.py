e3c347957d597dd18259067c819aa3590812f540
Durabun <eugenejon1@gmail.com>
2017-08-09 01:51:33 -0400

    Fleshed out functions, now plots

--- quantumWell.py
+++ quantumWell.py
@@ -1,8 +1,16 @@
+#QWell 0.5, the quantum well simulator!
+#Made by Jonathan Eugenio
+#And Mark Romero
+
+#Please refer to the license in the github repo (https://github.com/Durabun/QWell.git)
+
 import numpy as np
 from os import system
 from scipy.optimize import*
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import quantumFn
+from matplotlib import animation
 
 h = 6.626*10**(-34) #These are some constants
 h_bar = h/(2*np.pi) #Specifically, Planck's Constant
@@ -73,9 +81,6 @@ while x0 < R:
 		ie = ie +1
 		x0 = (1+2*jo)*np.pi/2 #Resets again
 
-print (evenRoot)
-print (oddRoot)
-
 #Initialize the propagation and tunneling vectors
 #At this point I consolidate the even and odd roots
 #Into single arrays instead of having arrays for 
@@ -83,72 +88,86 @@ print (oddRoot)
 propVector = np.zeros(S)
 tunnVector = np.zeros(S)
 
-i = 0
-j = 1
-
+ev = 0
+od = 1
+Roots = np.zeros(S)
 #This is to define the vectors of each function
-#THIS STILL DOES NOT WORK CORRECTLY
-#IT MESSES UP WHEN THE NUMBER OF SOLUTIONS INCREASES
-while i < evenRoot.size:
-	propVector[2*i] = (10**(-10))*evenRoot[i]/(a)
-	tunnVector[2*i] = (10**(-10))*(quantumFn.radFunction(evenRoot[i],R))/a
-	i = i +1
-while j-1 < oddRoot.size:
-	propVector[j] = (10**(-10))*oddRoot[j-1]/(a)
-	tunnVector[j] = ((10**(-10))*quantumFn.radFunction(oddRoot[j-1],R))/a
-	j = j + 2
+while ev < S:
+	Roots[ev] = evenRoot[int(ev/2)]
+	ev = ev +2
+
+while od < S:
+	Roots[od] = oddRoot[int(od/2)]
+	od = od + 2
+
+print (Roots)
+
+ev = 0
+od = 1
+
+while ev < S:
+	propVector[ev] = (10**(-10))*Roots[ev]/a
+	tunnVector[ev] = (10**(-10))*(quantumFn.radFunction(Roots[ev],R))/a
+	ev = ev + 2
+
+while od < S:
+	propVector[od] = (10**(-10))*Roots[od]/a
+	tunnVector[od] = (10**(-10))*(quantumFn.radFunction(Roots[od],R))/a
+	od = od + 2
 
 print ("Propagation 'k' vectors")
 print (propVector)
 print ("Tunneling 'K' vectors")
 print (tunnVector)
 
-#What needs to be done:
-#1. Correct the vector definition
-#2. Incorporate the definition of the B coefficients
-#3. Incorporate the rest of index_plot.py
+Bcoeff = np.zeros(S)
+StateEnergy = np.zeros(S)
 
+l = 0
+while l < S:
+	Bcoeff[l] = quantumFn.Beven(a*10**(10),propVector[l],tunnVector[l])
+	StateEnergy[l] = -1*quantumFn.energy(quantumFn.radFunction(Roots[l],R),R,V)
+	l = l + 2
+l = 1
+while l < S:
+	Bcoeff[l] = quantumFn.Bodd(a*10**(10),propVector[l],tunnVector[l])
+	StateEnergy[l] = -1*quantumFn.energy(quantumFn.radFunction(Roots[l],R),R,V)
+	l = l + 2
 
-#Bcoeff = np.zeros(S)
-
-Bcoeff = np.array([0.6318,0.617,0.4823])
-#l = 0
-#def B_Function(k,K)
-#while l < S:
-#	Bcoeff[l] = B_Function(prop[l],tunn[l])
-#	l = l +1
-
+print ("Coefficients")
 print (Bcoeff)
+print ("Energies")
+print (StateEnergy)
 
 z = 0
-r1 = np.arange(-5,(-a*10**(10)),0.001)
+r1 = np.arange((-5*a*10**(10)),(-a*10**(10)),0.001)
 r2 = np.arange((-a*10**(10)),(a*10**(10)),0.001)
-r3 = np.arange((a*10**(10)),5,0.001)
+r3 = np.arange((a*10**(10)),(5*a*10**(10)),0.001)
 color = ["r","b","g","c","m","y","k"] #This is the color array
 
+c = 0
 while z < S:
+	if c == 6:
+		c = 0
 	if z%2 == 1:
 		plt1 = quantumFn.of1(Bcoeff[z],tunnVector[z],propVector[z],(a*10**(10)))
 		plt2 = quantumFn.of2(Bcoeff[z],propVector[z])
 		plt3 = quantumFn.of3(Bcoeff[z],tunnVector[z],propVector[z],(a*10**(10)))
-
-		plt.plot(r1,plt1(r1),color[z],r2,plt2(r2),color[z],r3,plt3(r3),color[z])
+		
+		plt.plot(r1,plt1(r1)+StateEnergy[z],color[c],r2,plt2(r2)+StateEnergy[z],color[c],r3,plt3(r3)+StateEnergy[z],color[c])
+		
+		
 
 	else:
 		plt1 = quantumFn.ef1(Bcoeff[z],tunnVector[z],propVector[z],(a*10**(10)))
 		plt2 = quantumFn.ef2(Bcoeff[z],propVector[z])
 		plt3 = quantumFn.ef3(Bcoeff[z],tunnVector[z],propVector[z],(a*10**(10)))
 
-		plt.plot(r1,plt1(r1),color[z],r2,plt2(r2),color[z],r3,plt3(r3),color[z])
+		plt.plot(r1,plt1(r1)+StateEnergy[z],color[c],r2,plt2(r2)+StateEnergy[z],color[c],r3,plt3(r3)+StateEnergy[z],color[c])
+		
+		
 	z = z + 1
+	c = c + 1
 
-plt.show()	
-		
-		
-
-
-
-
-
-
-
+plt.xlabel("Distance (in Angstrom)")
+plt.show()
